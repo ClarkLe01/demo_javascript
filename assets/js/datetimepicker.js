9b93803df494b7cfeaa3a13d3d43e61230610411
waflol <lenguyenhunganh2001@gmail.com>
2022-09-28 14:43:42 +0700

    add search function

--- assets/js/datetimepicker.js
+++ assets/js/datetimepicker.js
@@ -1,18 +1,7 @@
-// $(function () {
-//     $("#datepicker-start").datepicker();
-// });
+
 flatpickr(".js-datepicker", {
     minDate: "today",
     altInput: true,
     altFormat: "d/m/Y",
     dateFormat: "Y-m-d",
 });
-
-
-// flatpickr("#datepicker-start", {
-//     minDate: "today",
-//     altInput: true,
-//     altFormat: "d/m/Y",
-//     dateFormat: "Y-m-d",
-// });
-
