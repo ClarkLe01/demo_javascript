22aafcd5f26054d23e9df163b149df20fc126630
waflol <lenguyenhunganh2001@gmail.com>
2022-09-29 17:42:17 +0700

    update custom pagination by using url data

--- assets/js/custom_pagination.js
+++ assets/js/custom_pagination.js
@@ -9,43 +9,39 @@ var pagination = (numRecord_per_page, Jsondata) => {
         }
         arrData_perPage.push(arr_temp);
     }
-    return arrData_perPage;
+    return [numPage,arrData_perPage];
 }
 
 var add_nav_pagination = (numPage, current_page = 1) => {
     var pagination = $('#pagination');
     pagination.empty();
+    var html = '';
     if (numPage <= 1) {
-        let html = `<li class="page-item"><a class="page-link" href="#">1</a></li>`;
-        pagination.append(html);
+        html = `<li class="page-item"><a class="page-link" href="./list_projects.html?page=1">1</a></li>`;
+        
 
     }
     else if (current_page == numPage) {
-        let html = `
+        html = `    
         <li class="page-item">
             <a class="page-link" href="./list_projects.html?page=${current_page - 1}" aria-label="Previous">
             <span aria-hidden="true">&laquo;</span>
             </a>
         </li>`;
         for (let i = 1; i <= numPage; i++) {
-            html += `<li class="page-item"><a class="page-link" href="page=${i}">${i}</a></li>`;
-        }
+            html += `<li class="page-item"><a class="page-link" href="./list_projects.html?page=${i}">${i}</a></li>`;
+        }   
     }
     else if (current_page == 1) {
-        let html = `
-        <li class="page-item">
-            <a class="page-link" href="./list_projects.html?page=${current_page + 1}" aria-label="Next">
-                <span aria-hidden="true">&raquo;</span>
-            </a>
-        </li>`;
-        for (let i = 1; i <= num; i++) {
-            html += `<li class="page-item"><a class="page-link" href="page=${i}">${i}</a></li>`;
+        for (let i = 1; i <= numPage; i++) {
+            html += `<li class="page-item"><a class="page-link" href="./list_projects.html?page=${i}">${i}</a></li>`;
         }
         html += `
         <li class="page-item">
-            <a class="page-link" href="./list_projects.html?page=${current_page1}" aria-label="Next">
+            <a class="page-link" href="./list_projects.html?page=${Number.parseInt(current_page)+1}" aria-label="Next">
             <span aria-hidden="true">&raquo;</span>
             </a>
         </li>`;
     }
+    pagination.append(html);
 }
--- assets/js/show_data.js
+++ assets/js/show_data.js
@@ -18,13 +18,16 @@ $(document).ready(() => {
 var add_data_table = (Jsondata)=>{
     var dataTableBody = $('table#project_data tbody');
     dataTableBody.empty();
+    var current_page = get_current_page();
+    var paging_data = pagination(5,Jsondata);
+    var data = paging_data[1][current_page-1];
     var drow = document.createElement('tr');
     dataTableBody.append(drow);
-    for(let i in Jsondata){
+    for(let i in data){
         console.log(i);
         var drow = document.createElement('tr');
         drow.setAttribute("id", `project_${i}`);
-        let df = new Project_html(Jsondata[i]);
+        let df = new Project_html(data[i]);
         drow.append(df.getTdId());
         drow.append(df.getTdName());
         drow.append(df.getDesc());
@@ -33,4 +36,15 @@ var add_data_table = (Jsondata)=>{
         drow.append(df.getAction());
         dataTableBody.append(drow);
     }
+    add_nav_pagination(paging_data[0],current_page);
+}
+
+
+var get_current_page = ()=>{
+    var queryString = location.search.substring(1);
+    if(queryString.length==0){
+        return 1;
+    }
+    var splited_qString = queryString.split("=");
+    return splited_qString[1];
 }
